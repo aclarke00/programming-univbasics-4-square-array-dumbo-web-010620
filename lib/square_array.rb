354e39089c51bb53f99aa65f9475cc25bc47f29b
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-12-19 22:01:31 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |numbers|
+    square = numbers * numbers
+    new_array.push(square)
+  end
+  return new_array
 end
+
+square_array(new_array)

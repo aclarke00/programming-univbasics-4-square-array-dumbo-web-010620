46270972705dd5dba1a77a4fcf18fa623ebbf38c
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-12-19 21:53:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,5 +2,7 @@ def square_array(array)
   new_array = []
   array.each do |numbers|
     square = numbers * numbers
-    new_array.push(numbers)
+    new_array.push(square)
+  end
+  return new_array
 end
